e8853c04db77fce0ef4dcb80f46d28ae697a13c0
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-01-18 15:54:54 -0500

    creating HTML and routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,9 @@
 var app = require('express');
 
-var friends = [
+var friends = [{
+    name: "Bebop",
+    image: "https://vignette.wikia.nocookie.net/tmnt/images/d/dd/1832751-3725262051_582e857296_o.png/revision/latest?cb=20130919192742",
+}
 
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,10 @@
+var app = require('express');
 var friends = require('/data/friends');
 
-friends.push(userData);
+app.get("/api/friends", function (req, res) {
+    // res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.post("/api/friends", function (req, res) {
+    // res.sendFile(path.join(__dirname, "view.html"));
+});
