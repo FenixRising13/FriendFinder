a9f9a9f8a48d3a88c18c2fabd259d9dbbe1f6e11
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-01-19 09:15:46 -0500

    Connection to htmlRoutes is failing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,12 @@
 var path = require("path");
-// var app = require("express");
 
 module.exports = function (app) {
+
+    // Uses the path /new to add entries to friends object
     app.post("/api/new", function (req, res) {
-        // req.body hosts is equal to the JSON post sent from the user
-        // This works because of our body-parser middleware
+        
         var newfriend = req.body;
-        // Using a RegEx Pattern to remove spaces from newCharacter
-        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+
         newfriend.name = newfriend.name.replace(/\s+/g, "").toLowerCase();
 
         console.log(newfriend);
@@ -16,7 +15,8 @@ module.exports = function (app) {
         res.json(newfriend);
 
     });
-
+    
+//  Returns results when client accesses GET friends
     app.get("/api/friends", function (req, res) {
         var chosen = req.params.friends;
 
@@ -32,4 +32,5 @@ module.exports = function (app) {
         }
         return res.json(friends);
     });
+
 }
